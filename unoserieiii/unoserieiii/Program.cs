1ef1314d2f58e432daa56f99bae9251224464a1c
majitomoomin <mariajoseportill2@gmail.com>
2024-03-09 12:22:44 -0600

    Agregar archivos de proyecto.

---
+++ unoserieiii/unoserieiii/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Program;
+
+public static void Main(string[]args)
+{
+    Console.Write("Bienvenido");
+    Console.WriteLine("Ingrese su gmail sin el @gmail.com");
+    String gmail = Console.ReadLine();
+
+}
