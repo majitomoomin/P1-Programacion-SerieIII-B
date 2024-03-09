bec3a540af933f31568436e5566291d665a5bb2d
majitomoomin <mariajoseportill2@gmail.com>
2024-03-09 12:45:54 -0600

    commit

---
+++ segundo/segundo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Program;
+
+class programa
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Bienvenido");
+        Console.WriteLine("Ingrese el precio de su primer producto);
+        int primer = Console.ReadLine();
+        Console.WriteLine("Ingrese el precio del segundo producto");
+        int segundo = Console.ReadLine();
+        Console.WriteLine("Ingrese el precio del tercero");
+        int tercero = Console.ReadLine();
+
+        if (primer => 100)
+        {
+            descuento = primer * 0.15;
+            Console.WriteLine("se le hizo un descuento del 15%");
+        }
+
+    }
+}
