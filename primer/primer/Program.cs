3a0f003865b348a51f9f27f10cd1ff9073e4153a
majitomoomin <mariajoseportill2@gmail.com>
2024-03-09 12:32:12 -0600

    primer commit

--- primer/primer/Program.cs
+++ primer/primer/Program.cs
@@ -2,7 +2,7 @@
 
 namespace Program;
 
-public static void Main(string[]args)
+public static void Main(string[] args)
 {
     Console.Write("Bienvenido");
     Console.WriteLine("Ingrese su gmail sin el @gmail.com");
